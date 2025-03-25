c902b18ca16d6fe31360e005762eab9e50ed3129
kaveen-k02 <kaveen3k@gmail.com>
2025-03-25 09:25:41 +0530

    updating Shopping List Models

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -37,10 +37,7 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is up and running on port number: ${PORT}`);
 });
-<<<<<<< Updated upstream
 
 
 
 
-=======
->>>>>>> Stashed changes
